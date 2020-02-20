a533399c56110be47cb84502808c79630253d10a
NGUYEN Vo Vinh Nghi <vogi.nguyen@live.fr>
2020-02-20 18:01:59 +0100

    react state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ContacList from './components/ContactList';
 
-let isConnected = true;
-
 function App() {
   return (
     <div className='App'>
-      <ContacList status={isConnected} />
+      <ContacList />
     </div>
   );
 }
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,28 +1,36 @@
 import React from 'react';
 import './Contact.css';
 
-const status = {
-    online : <div className="status-online" />,
-    offline : <div className="status-offline"/>
-}
+class Contact extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            online : props.online
+        };
+    }
 
-function Contact(props) {
-  return(
-    <div className="Contact">
-        <img className="avatar" src={props.image} alt="" />
-        <div className="infos">
-            <div className="name">
-                <h3>{props.name}</h3>
-            </div>
-            <div className="status">
-                {props.status ?  status.online : status.offline }
-                <div className="status-text">
-                    {props.statusText}
+    handleClick = (e) => {
+        this.setState({online : !this.state.online});
+    };
+
+    render () {
+        return(
+            <div className="Contact">
+                <img className="avatar" src={this.props.image} alt="" />
+                <div className="infos">
+                    <div className="name">
+                        <h3>{this.props.name}</h3>
+                    </div>
+                    <div className="status">
+                        <span onClick={this.handleClick}>{this.state.online ?  <div className="status-online"/> : <div className="status-offline"/> }</span>
+                        <div className="status-text">
+                            {this.state.online ? 'Online' : 'Offline'}
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
-  );  
+        );  
+    }
 }
 
 export default Contact;
